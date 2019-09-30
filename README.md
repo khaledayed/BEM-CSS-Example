03a7d8e59f4957db04e15dc2ef054e06c3b6f310
Khaled Ayed <ayed.khaled@gmail.com>
2019-10-01 00:04:31 +0100

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+### Example of use BEM CSS Naming (Block Element Modifier) to Integration websites.
+
+##BEM
+>BEM is a design methodology that helps you to create reusable components and code sharing in front-end development. — getbem.com
+
+##Run project
+```js
+npm install
+npm run build
+```
+##Mockup
+
+![mockup](media/mockup.png)
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require("gulp"),
+    del = require("del"),
+    sass = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    cleancss = require("gulp-clean-css"),
+    rename = require("gulp-rename");
+
+const compile = () => {
+    return (
+        gulp
+            .src("./sass/style.scss")
+            .pipe(
+                sass({
+                    includePaths: ["./node_modules"]
+                })
+            )
+            .on("error", sass.logError)
+            .pipe(
+                autoprefixer()
+            )
+            // Uncomment in case you need/want to minify your final CSS
+            /*
+            .pipe(cleancss({ rebase: false, level: { 1: { specialComments: 0 } } }))
+            .pipe(rename({ suffix: ".min" }))
+            */
+            .pipe(gulp.dest("./css"))
+    );
+};
+
+const clean = () => {
+    return del(["./css/*.css"]);
+};
+
+exports.sass = compile;
+exports.clean = clean;
+
+var build = gulp.series(clean, compile);
+
+
+gulp.task('watch', () => {
+    gulp.watch('**/*.scss', (done) => {
+        build(done);
+    })
+});
+
+gulp.task("default", build);
